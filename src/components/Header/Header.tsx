606ca72c3306d8814d7f314534b7de0a1e01dfe2
Viktoriia <struk.victoria@gmail.com>
2025-10-05 02:07:40 +0300

    add header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { useTranslations } from "next-intl";
+import styles from "./Header.module.css";
+import LanguageSelector from "../LanguageSelector/LanguageSelector";
 
 const Header = () => {
-	return <div>Header</div>;
+  const t = useTranslations("Header");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const menuItems = [
+    { key: "portfolio", href: "#portfolio" },
+    { key: "services", href: "#services" },
+    { key: "reviews", href: "#reviews" },
+    { key: "blog", href: "#blog" },
+    { key: "contacts", href: "#contacts" },
+  ];
+
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.desktop} container`}>
+        <a href="/">
+          <img src="/img/logo.svg" alt="logo" className={styles.logoDesktop} />
+        </a>
+
+        <nav className={styles.navDesktop}>
+          <ul className={styles.menuListDesktop}>
+            {menuItems.map(({ key, href }) => (
+              <li key={key}>
+                <a href={href}>{t(`menu.${key}`)}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className={styles.headerRightDesktop}>
+          <LanguageSelector />
+          <a className={styles.btnDesktop} href="#">
+            {t("button")}
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.headerRightMobile}>
+        <LanguageSelector />
+        <button
+          type="button"
+          className={`${styles.burger} ${menuOpen ? styles.open : ""}`}
+          onClick={toggleMenu}
+          aria-label={menuOpen ? "Close menu" : "Open menu"}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+
+      {menuOpen && (
+        <nav className={`${styles.mobileMenu} ${menuOpen ? styles.show : ""}`}>
+          <a href="#">
+            <img src="/img/logo.svg" alt="logo" className={styles.logoMobile} />
+          </a>
+          <ul className={styles.menuList}>
+            {menuItems.map(({ key, href }) => (
+              <li key={key}>
+                <a href={href}>{t(`menu.${key}`)}</a>
+              </li>
+            ))}
+          </ul>
+          <a className={styles.btnMobile} href="#">
+            {t("button")}
+          </a>
+        </nav>
+      )}
+    </header>
+  );
 };
 
 export default Header;
