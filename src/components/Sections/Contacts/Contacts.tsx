f4964f229e6518dcf549e3a275b09c5007f42238
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-10-16 18:46:43 +0300

    first-sett-css

--- src/components/Sections/Contacts/Contacts.tsx
+++ src/components/Sections/Contacts/Contacts.tsx
@@ -3,6 +3,7 @@ import { FormData, sendEmail } from "@/api/sendMessage";
 import { useTranslations } from "next-intl";
 import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import s from "./Contacts.module.css";
 
 const Contacts = () => {
   const t = useTranslations("Contacts.form");
@@ -27,46 +28,48 @@ const Contacts = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      style={{ maxWidth: 400, margin: "0 auto" }}
-    >
-      <label>
-        {t("nameLabel")}
-        <input
-          {...register("name", { required: t("requiredName") })}
-          placeholder={t("namePlaceholder")}
-        />
-        {errors.name && <p>{errors.name.message}</p>}
-      </label>
+    <div className={s.container}>
+      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+        <label className={s.contInput}>
+          {t("nameLabel")}
+          <input
+            className={s.inputCont}
+            {...register("name", { required: t("requiredName") })}
+            placeholder={t("namePlaceholder")}
+          />
+          {errors.name && <p>{errors.name.message}</p>}
+        </label>
 
-      <label>
-        {t("numberLabel")}
-        <input
-          {...register("number", {
-            required: t("requiredNumber"),
-            pattern: { value: /^[0-9+\s-]+$/, message: t("invalidNumber") },
-          })}
-          placeholder={t("numberPlaceholder")}
-        />
-        {errors.number && <p>{errors.number.message}</p>}
-      </label>
+        <label className={s.contInput}>
+          {t("numberLabel")}
+          <input
+            className={s.inputCont}
+            {...register("number", {
+              required: t("requiredNumber"),
+              pattern: { value: /^[0-9+\s-]+$/, message: t("invalidNumber") },
+            })}
+            placeholder={t("numberPlaceholder")}
+          />
+          {errors.number && <p>{errors.number.message}</p>}
+        </label>
 
-      <label>
-        {t("messageLabel")}
-        <textarea
-          {...register("message")}
-          placeholder={t("messagePlaceholder")}
-        />
-      </label>
+        <label className={s.contInput}>
+          {t("messageLabel")}
+          <textarea
+            className={s.textareaCont}
+            {...register("message")}
+            placeholder={t("messagePlaceholder")}
+          />
+        </label>
 
-      <button type="submit" disabled={isSubmitting}>
-        {isSubmitting ? t("sending") : t("submit")}
-      </button>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? t("sending") : t("submit")}
+        </button>
 
-      {status === "success" && <p>{t("success")}</p>}
-      {status === "error" && <p>{t("error")}</p>}
-    </form>
+        {status === "success" && <p>{t("success")}</p>}
+        {status === "error" && <p>{t("error")}</p>}
+      </form>
+    </div>
   );
 };
 export default Contacts;
