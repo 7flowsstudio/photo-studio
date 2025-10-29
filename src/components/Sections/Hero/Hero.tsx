bded98b543961e50c681ebfeb0d243d3f26a06f4
Viktoriia <struk.victoria@gmail.com>
2025-10-29 13:52:44 +0200

    Optimize background images and styles

--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -1,35 +1,59 @@
-import React from "react";
+import Image from "next/image";
+import bgMobile from "../../../../public/img/hero/background-mobile.png";
+import bgDesktop from "../../../../public/img/hero/background.png";
 import { useTranslations } from "next-intl";
-
 import styles from "./Hero.module.css";
 
-const Hero = () => {
+export default function Hero() {
   const t = useTranslations("Hero");
+
   return (
     <section className={styles.hero}>
-      <div className="container">
+      <Image
+        src={bgMobile}
+        fill
+        className={styles.bgMobile}
+        sizes="(max-width: 767px) 100vw"
+        quality={60}
+        placeholder="blur"
+        priority
+        alt=""
+      />
 
-      <div className={styles.wrapperInfo}>
-        <div className={styles.content}>
-          <span className={`${styles.bage} ${styles.desktop}`}>
-            {t("bage")}
-          </span>
-          <h1 className={styles.title}>{t("title")}</h1>
-          <p className={styles.details}>{t("details")}</p>
-          <a href="#contacts" className={`${styles.btn} ${styles.desktop}`}>
-            {t("button")}
-          </a>
+      <Image
+        src={bgDesktop}
+        alt=""
+        fill
+        sizes="(min-width: 768px) 100vw"
+        quality={75}
+        className={styles.bgDesktop}
+      />
+
+      <div className={styles.overlay}></div>
+
+      <div className="container">
+        <div className={styles.wrapperInfo}>
+          <div className={styles.content}>
+            <span className={`${styles.bage} ${styles.desktop}`}>
+              {t("bage")}
+            </span>
+            <h1 className={styles.title}>{t("title")}</h1>
+            <p className={styles.details}>{t("details")}</p>
+            <a href="#contacts" className={`${styles.btn} ${styles.desktop}`}>
+              {t("button")}
+            </a>
+          </div>
+
+          <div className={styles.contentMobileBotom}>
+            <span className={`${styles.bage} ${styles.mobile}`}>
+              {t("bage")}
+            </span>
+            <a href="#contacts" className={`${styles.btn} ${styles.mobile}`}>
+              {t("button")}
+            </a>
+          </div>
         </div>
-        <div className={styles.contentMobileBotom}>
-          <span className={`${styles.bage} ${styles.mobile}`}>{t("bage")}</span>
-          <a href="#contacts" className={`${styles.btn} ${styles.mobile}`}>
-            {t("button")}
-          </a>
-        </div>
-      </div>
       </div>
     </section>
   );
-};
-
-export default Hero;
+}
