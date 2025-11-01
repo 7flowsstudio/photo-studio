2e017b0da32dd4ecc2d6f43dda9eae3e9f5d42b9
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-11-01 12:28:12 +0200

    first-sett-footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,81 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 import React from "react";
+import s from "./Footer.module.css";
 
 const Footer = () => {
-	return <div>Footer</div>;
+  const t = useTranslations("Footer");
+  return (
+    <div>
+      <h3>{t("title")}</h3>
+      <div className={s.wrappImg}>
+        <Image
+          src="/img/footer/footer.png"
+          alt="Image footer"
+          width={200}
+          height={200}
+          sizes="(max-width: 768px) 375px, 432px"
+          style={{ width: "100%", height: "100%" }}
+          priority
+        />
+      </div>
+      <ul>
+        <li>
+          {" "}
+          <Image
+            src="/img/icons/icon-tel.svg"
+            alt="tel"
+            width={24}
+            height={24}
+          />
+          +380 97 497 14 78
+        </li>
+        <li>
+          {" "}
+          <Image
+            src="/img/icons/icon-mail.svg"
+            alt="mail"
+            width={24}
+            height={24}
+          />
+          kobrynkhrystyna@gmail.com
+        </li>
+        <li>
+          {" "}
+          <Image
+            src="/img/icons/icon-map.svg"
+            alt="map"
+            width={24}
+            height={24}
+          />
+          {t("map")}
+        </li>
+      </ul>
+      <div>
+        <Image
+          src="/img/icons/icon-inst.svg"
+          alt="instagram"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/img/icons/icon-faceb.svg"
+          alt="facebook"
+          width={24}
+          height={24}
+        />
+      </div>
+      <Image
+        src="/img/footer/insert-desc.png"
+        alt="Image footer"
+        width={200}
+        height={200}
+        sizes="(max-width: 768px) 375px, 432px"
+        style={{ width: "100%", height: "100%" }}
+        priority
+      />
+    </div>
+  );
 };
 
 export default Footer;
