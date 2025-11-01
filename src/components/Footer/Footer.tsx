c912be0e6ccb0690054355206ae2dbeb21e87bf0
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-11-01 12:38:32 +0200

    css-footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,63 +7,65 @@ const Footer = () => {
   const t = useTranslations("Footer");
   return (
     <div>
-      <h3>{t("title")}</h3>
-      <div className={s.wrappImg}>
-        <Image
-          src="/img/footer/footer.png"
-          alt="Image footer"
-          width={200}
-          height={200}
-          sizes="(max-width: 768px) 375px, 432px"
-          style={{ width: "100%", height: "100%" }}
-          priority
-        />
-      </div>
-      <ul>
-        <li>
-          {" "}
+      <div className={s.containerFooter}>
+        <h3>{t("title")}</h3>
+        <div className={s.wrappImg}>
           <Image
-            src="/img/icons/icon-tel.svg"
-            alt="tel"
-            width={24}
-            height={24}
+            src="/img/footer/footer.png"
+            alt="Image footer"
+            width={200}
+            height={200}
+            sizes="(max-width: 768px) 375px, 432px"
+            style={{ width: "100%", height: "100%" }}
+            priority
           />
-          +380 97 497 14 78
-        </li>
-        <li>
-          {" "}
+        </div>
+        <ul className={s.list}>
+          <li className={s.element}>
+            {" "}
+            <Image
+              src="/img/icons/icon-tel.svg"
+              alt="tel"
+              width={24}
+              height={24}
+            />
+            +380 97 497 14 78
+          </li>
+          <li className={s.element}>
+            {" "}
+            <Image
+              src="/img/icons/icon-mail.svg"
+              alt="mail"
+              width={24}
+              height={24}
+            />
+            kobrynkhrystyna@gmail.com
+          </li>
+          <li className={s.element}>
+            {" "}
+            <Image
+              src="/img/icons/icon-map.svg"
+              alt="map"
+              width={24}
+              height={24}
+            />
+            {t("map")}
+          </li>
+        </ul>
+        <div className={s.wrappSocMedia}>
           <Image
-            src="/img/icons/icon-mail.svg"
-            alt="mail"
-            width={24}
-            height={24}
+            src="/img/icons/icon-inst.svg"
+            alt="instagram"
+            width={36}
+            height={36}
           />
-          kobrynkhrystyna@gmail.com
-        </li>
-        <li>
-          {" "}
           <Image
-            src="/img/icons/icon-map.svg"
-            alt="map"
-            width={24}
-            height={24}
+            src="/img/icons/icon-faceb.svg"
+            alt="facebook"
+            width={36}
+            height={36}
           />
-          {t("map")}
-        </li>
-      </ul>
-      <div>
-        <Image
-          src="/img/icons/icon-inst.svg"
-          alt="instagram"
-          width={24}
-          height={24}
-        />
-        <Image
-          src="/img/icons/icon-faceb.svg"
-          alt="facebook"
-          width={24}
-          height={24}
-        />
+        </div>
       </div>
       <Image
         src="/img/footer/insert-desc.png"
